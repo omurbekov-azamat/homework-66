d92e31a97a663934ab85585f662a7b3252feb7ef
omurbekov-azamat <avgusttin8@gmail.com>
2022-12-10 15:14:43 +0600

    added NavBar.tsx;

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-primary bg-opacity-50">
+      <div className="container p-2">
+        <p className="navbar-brand m-0 me-5">Calorie tracker</p>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
